07d000593b654918c02a27cfdd37eed332ba7b5c
lenadraz <128379882+lenadraz@users.noreply.github.com>
2026-04-15 22:50:03 +0300

    Update UserPrivacyView.vue

--- src/views/UserPrivacyView.vue
+++ src/views/UserPrivacyView.vue
@@ -18,7 +18,6 @@
 </template>
 
 <script setup>
-import UserLayout from "../../components/user/UserLayout.vue";
 import { userPrivacyPolicy as policy } from "../../content/userPrivacyPolicy";
 </script>
 
