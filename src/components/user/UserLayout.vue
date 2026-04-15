99dc210bff1a9a6eae2d68885a3c91bca41aed2e
lenadraz <128379882+lenadraz@users.noreply.github.com>
2026-04-15 22:54:42 +0300

    Create UserLayout.vue

---
+++ src/components/user/UserLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <TopNav />
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup>
+import TopNav from "@/components/TopNav.vue"
+</script>
+
+<style scoped>
+main {
+  padding: 20px;
+}
+</style>
